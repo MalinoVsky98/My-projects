c6d29e40957b7754b3b2a48027f5a11bc9d7483f
MalinoVsky98 <piotrmalinowski94@gmail.com>
2016-09-30 18:47:53 +0200

    Add files via upload

---
+++ Dzienniczek.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Dzienniczek {
+  public static void main(String[]args) {
+    ArrayList<Integer> matematyka = new ArrayList<Integer>();
+    ArrayList<Integer> polski = new ArrayList<Integer>();
+    ArrayList<Integer> angielski = new ArrayList<Integer>();
+    matematyka.add(5);
+    polski.add(5);
+    angielski.add(5);
+    
+    System.out.println("Oto twój ma³y dzienniczek ucznia");
+    System.out.println("Wybierz przedmiot aby wyœwietliæ lub dodaæ oceny");
+    System.out.println("1.Matematyka");
+    System.out.println("2.Polski");
+    System.out.println("3.Angielski"); //wypisujemy nazwy przedmiotów
+    
+    int zero = 0;
+    Scanner tekst = new Scanner(System.in);
+    int cyfra = tekst.nextInt();
+    System.out.println("Wybra³eœ numer" + cyfra);
+    
+    	if(cyfra==1){
+    		int mr = matematyka.size();
+    		System.out.println("1.Dodawanie ocen");
+    		System.out.println("2.Wyœwitlanie ocen");
+    		int cyfraa = tekst.nextInt();
+    			if(cyfraa==1){
+    				System.out.println("Podaj ocenê któr¹ chesz dodaæ do dzienniczka");
+    					int ocena = tekst.nextInt();
+    					matematyka.add(ocena);
+    						
+    			}else {
+    				System.out.println("Oto twoje oceny z matematyki: ");
+						for(int i = 0;i==mr;i++){
+					System.out.println(matematyka.get(i));
+					
+						}
+    			}
+    		}
+
+		if(cyfra==2){
+			int mr = polski.size();
+			System.out.println("1.Dodawanie ocen");
+			System.out.println("2.Wyœwitlanie ocen");
+			int cyfraa = tekst.nextInt();
+				if(cyfraa==1){
+					System.out.println("Podaj ocenê któr¹ chesz dodaæ do dzienniczka");
+						int ocena = tekst.nextInt();
+						polski.add(ocena);
+							
+				}else {
+					System.out.println("Oto twoje oceny z polskiego" );
+						for(int i = 0;i==mr;i++){
+					System.out.println(polski.get(i));
+						}
+				}
+		}
+					if(cyfra==3){
+			    		int mr = angielski.size();
+			    		System.out.println("1.Dodawanie ocen");
+			    		System.out.println("2.Wyœwitlanie ocen");
+			    		int cyfraa = tekst.nextInt();
+			    			if(cyfraa==1){
+			    				System.out.println("Podaj ocenê któr¹ chesz dodaæ do dzienniczka");
+			    					int ocena = tekst.nextInt();
+			    					angielski.add(ocena);
+			    						
+			    			}else {
+			    				System.out.println("Oto twoje oceny z angielskiego: ");
+									for(int i = 0;i==mr;i++){
+								System.out.println(angielski.get(i));
+									}							
+			    			}
+					}
+  }
+}  
